e6a83e154547425f824548c376a330bc00242970
Jean28518 <jf_v@gmx.de>
2024-01-07 13:35:08 +0100

    Add cool graphics to install iso and remove grub menu at .iso boot #7

--- build_iso.sh
+++ build_iso.sh
@@ -7,15 +7,38 @@ mount -o loop debian-original.iso ./ORIGISO
 cp -rT ./ORIGISO/ ./NEWISO/
 umount ./ORIGISO 
 
-# Write preseed.cfg into new iso
+# Write preseed.cfg and graphic into new iso
 chmod +w -R ./NEWISO/install.$arch/
 gunzip ./NEWISO/install.$arch/gtk/initrd.gz
 echo preseed.cfg | cpio -H newc -o -A -F ./NEWISO/install.$arch/gtk/initrd
+
+# Extraxt initrd.gz
+cd ./NEWISO/install.$arch/gtk/
+mkdir tmp && cd tmp
+cpio -i -F ../initrd -d 
+
+# Overwrite libre workspace header with debian header
+cd ../../../../
+cp libre_workspace_header.png ./NEWISO/install.$arch/gtk/tmp/usr/share/graphics/logo_debian.png
+cd ./NEWISO/install.$arch/gtk/tmp
+
+# Repack initrd.gz
+rm ../initrd
+find . | cpio -o -H newc -F ../initrd
+cd ../../../../
+rm -rf ./NEWISO/install.$arch/gtk/tmp
 gzip ./NEWISO/install.$arch/gtk/initrd
 chmod -w -R ./NEWISO/install.$arch
 
 # Copy the custom package (which does the installation of linux-arbeitsplatz in the end) to the new iso
 cp linux-arbeitsplatz.deb ./NEWISO/
+
+# Change the boot menu to automatically start the installation
+sed -i "s/timeout 300/timeout 1/g" ./NEWISO/isolinux/spkgtk.cfg
+sed -i "s/speakup.synth=soft //g" ./NEWISO/isolinux/spkgtk.cfg
+
+# Change the installer graphic to the one from libre workspace
+cp splash.png ./NEWISO/isolinux/splash.png
 
 # Generate new md5sums
 chmod +w ./NEWISO/md5sum.txt
