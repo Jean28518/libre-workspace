dee4ae0a55b743ffefe138510060b85aa8accdef
Jean28518 <jf_v@gmx.de>
2024-02-14 09:48:04 +0100

    Fix update script paths

--- src/lac/unix/unix_scripts/maintenance/update_everything.sh
+++ src/lac/unix/unix_scripts/maintenance/update_everything.sh
@@ -21,32 +21,32 @@ fi
 
 if [ "$NEXTCLOUD_AUTOMATIC_UPDATES" == "True" ]; then
     echo "Starting nextcloud update at $DATE" >> ../history/update-$DATE.log 2>&1
-    bash nextcloud/update_nextcloud.sh >> ../history/update-$DATE.log 2>&1
+    bash ../nextcloud/update_nextcloud.sh >> ../history/update-$DATE.log 2>&1
 fi
 
 if [ "$ONLYOFFICE_AUTOMATIC_UPDATES" == "True" ]; then
     echo "Starting onlyoffice update at $DATE" >> ../history/update-$DATE.log 2>&1
-    bash onlyoffice/update_onlyoffice.sh >> ../history/update-$DATE.log 2>&1
+    bash ../onlyoffice/update_onlyoffice.sh >> ../history/update-$DATE.log 2>&1
 fi
 
 if [ "$COLLABORA_AUTOMATIC_UPDATES" == "True" ]; then
     echo "Starting collabora update at $DATE" >> ../history/update-$DATE.log 2>&1
-    bash collabora/update_collabora.sh >> ../history/update-$DATE.log 2>&1
+    bash ../collabora/update_collabora.sh >> ../history/update-$DATE.log 2>&1
 fi
 
 if [ "$ROCKETCHAT_AUTOMATIC_UPDATES" == "True" ]; then
     echo "Starting rocketchat update at $DATE" >> ../history/update-$DATE.log 2>&1
-    bash rocketchat/update_rocketchat.sh >> ../history/update-$DATE.log 2>&1
+    bash ../rocketchat/update_rocketchat.sh >> ../history/update-$DATE.log 2>&1
 fi
 
 if [ "$MATRIX_AUTOMATIC_UPDATES" == "True" ]; then
     echo "Starting matrix update at $DATE" >> ../history/update-$DATE.log 2>&1
-    bash matrix/update_matrix.sh >> ../history/update-$DATE.log 2>&1
+    bash ../matrix/update_matrix.sh >> ../history/update-$DATE.log 2>&1
 fi
 
 if [ "$JITSI_AUTOMATIC_UPDATES" == "True" ]; then
     echo "Starting jitsi update at $DATE" >> ../history/update-$DATE.log 2>&1
-    bash jitsi/update_jitsi.sh >> ../history/update-$DATE.log 2>&1
+    bash ../jitsi/update_jitsi.sh >> ../history/update-$DATE.log 2>&1
 fi
 
 # Update all addons which are located in /usr/share/linux-arbeitsplatz/unix/unix_scripts/addons
@@ -56,7 +56,7 @@ for ADDON in /usr/share/linux-arbeitsplatz/unix/unix_scripts/addons/*; do
         ADDON_NAME=$(basename $ADDON)
         if [ "$(eval "echo \$${ADDON_NAME}_AUTOMATIC_UPDATES")" == "True" ]; then
             echo "Starting update of $ADDON_NAME at $DATE" >> ../history/update-$DATE.log 2>&1
-            bash "cd $ADDON; bash $ADDON/update_$ADDON_NAME.sh" >> ../history/update-$DATE.log 2>&1
+            bash "cd ../addons/$ADDON; bash update_$ADDON_NAME.sh" >> ../history/update-$DATE.log 2>&1
         fi
     fi
 done
