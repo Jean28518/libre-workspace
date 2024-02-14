61581153cf5ffbcbe247ba30925e589b34ea39e5
Jean3219 <jf_v@gmx.de>
2024-02-14 17:34:30 +0100

    Update update_matrix.sh

--- src/lac/unix/unix_scripts/matrix/update_matrix.sh
+++ src/lac/unix/unix_scripts/matrix/update_matrix.sh
@@ -1,8 +1,6 @@
-cd /root/matrix
-
 # Update Matrix
-docker-compose pull
-docker-compose up -d
+docker-compose -f /root/matrix/docker-compose.yml pull
+docker-compose -f /root/matrix/docker-compose.yml up -d
 
 . ../env.sh
 . update_element_config.sh
