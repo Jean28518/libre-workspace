28f51f42983142e6177e05fa123e510aa8db50ff
Jean28518 <jf_v@gmx.de>
2024-03-01 20:03:56 +0100

    Seperate /data backup from the rest #66

--- src/lac/unix/unix_scripts/maintenance/do_backup.sh
+++ src/lac/unix/unix_scripts/maintenance/do_backup.sh
@@ -28,20 +28,44 @@ fi
 
 
 # If $REMOTEPATH is set, then use this command:
-# If $REMOTEPATH is set, then use this command:
 if [ -z "$REMOTEPATH" ] ; then
-  borg create --exclude-caches $BORG_REPOSITORY::$DATE / -e /dev -e /proc -e /sys -e /tmp -e /run -e /media -e /mnt -e /var/log -e /var/lib/mysql/ib_logfile0 2> ../history/borg_errors_$DATE.log
+  borg create --exclude-caches $BORG_REPOSITORY::$DATE-system / -e /dev -e /proc -e /sys -e /tmp -e /run -e /media -e /mnt -e /var/log -e /var/lib/mysql/ib_logfile0 -e /data 2> ../history/borg_errors_$DATE.log
 else
-  borg create --remote-path $REMOTEPATH --exclude-caches $BORG_REPOSITORY::$DATE / -e /dev -e /proc -e /sys -e /tmp -e /run -e /media -e /mnt -e /var/log -e /var/lib/mysql/ib_logfile0 2> ../history/borg_errors_$DATE.log
+  borg create --remote-path $REMOTEPATH --exclude-caches $BORG_REPOSITORY::$DATE-system / -e /dev -e /proc -e /sys -e /tmp -e /run -e /media -e /mnt -e /var/log -e /var/lib/mysql/ib_logfile0 -e /data 2> ../history/borg_errors_$DATE.log
 fi
 
 # Start all services
 bash ./start_services.sh
 
-borg prune -v $BORG_REPOSITORY \
+# Prune old system backups
+borg prune -v --glob-archives '*-system' $BORG_REPOSITORY \
     --keep-daily=$BORG_KEEP_DAILY \
     --keep-weekly=$BORG_KEEP_WEEKLY \
-    --keep-monthly=$BORG_KEEP_MONTHLY \
+    --keep-monthly=$BORG_KEEP_MONTHLY
+
+
+
+# Then backup the /data directory while the services are running, because it is a big directory and takes a long time to backup
+# Also there are no databases in /data, so it is not a problem to backup it after the critical services
+if [ -d /data ]; then
+  if [ -z "$REMOTEPATH" ] ; then
+    borg create --exclude-caches $BORG_REPOSITORY::$DATE-data /data 2>> ../history/borg_errors_$DATE.log
+  else
+    borg create --remote-path $REMOTEPATH --exclude-caches $BORG_REPOSITORY::$DATE-data /data 2>> ../history/borg_errors_$DATE.log
+  fi
+  # BORG_KEEP_DAILY=$((2*$BORG_KEEP_DAILY))
+  # BORG_KEEP_WEEKLY=$((2*$BORG_KEEP_WEEKLY))
+  # BORG_KEEP_MONTHLY=$((2*$BORG_KEEP_MONTHLY))
+  # Prune old backups
+  borg prune -v --glob-archives '*-data' $BORG_REPOSITORY \
+    --keep-daily=$BORG_KEEP_DAILY \
+    --keep-weekly=$BORG_KEEP_WEEKLY \
+    --keep-monthly=$BORG_KEEP_MONTHLY
+fi
+
+# # Delete old backups which are older than $MAXIMUM_AGE_IN_DAYS and are falling through the regular pruning e.g. because of other naming
+MAXIMUM_AGE_IN_DAYS=$((31*$BORG_KEEP_MONTHLY))d
+borg prune -v --keep-within=$MAXIMUM_AGE_IN_DAYS $BORG_REPOSITORY
 
 
 borg list --short $BORG_REPOSITORY > ../history/borg_list
