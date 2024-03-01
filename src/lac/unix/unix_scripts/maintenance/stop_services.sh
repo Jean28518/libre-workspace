8801615d5ce409f4092951749050e2037680d22f
Jean28518 <jf_v@gmx.de>
2024-03-01 20:45:09 +0100

    Improve backup handling

--- src/lac/unix/unix_scripts/maintenance/stop_services.sh
+++ src/lac/unix/unix_scripts/maintenance/stop_services.sh
@@ -1,7 +1,8 @@
-systemctl stop docker
 . umount_backups.sh
 
 # Turn off maintenance mode of Nextcloud
 if [ -f /var/www/nextcloud/occ ]; then
     sudo -u www-data /usr/bin/php /var/www/nextcloud/occ maintenance:mode --on
 fi
+
+systemctl stop docker
