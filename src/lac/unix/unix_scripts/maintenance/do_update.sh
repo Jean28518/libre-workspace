6cd77f28ad273fce7a3c2fda506fb17e818551b0
Jean28518 <jf_v@gmx.de>
2024-04-18 04:43:34 +0200

    Fix apt script mode in update script

--- src/lac/unix/unix_scripts/maintenance/do_update.sh
+++ src/lac/unix/unix_scripts/maintenance/do_update.sh
@@ -10,9 +10,9 @@ CURRENT_TIME=`date +"%Y-%m-%d_%H-%M-%S"`
 
 echo "$CURRENT_TIME: Starting update..." > ../history/update.log
 
-apt update >> ../history/update.log
+apt-get update >> ../history/update.log
 
-DEBIAN_FRONTEND=noninteractive apt dist-upgrade -y >> ../history/update.log
+DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y >> ../history/update.log
 
 # Start all services
 bash ./start_services.sh
