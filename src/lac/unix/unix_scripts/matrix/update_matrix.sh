9ad55b335f602cca23b2d01269da2002b1150783
Jean28518 <jf_v@gmx.de>
2024-04-19 21:24:09 +0200

    Fix automatic update_element config call

--- src/lac/unix/unix_scripts/matrix/update_matrix.sh
+++ src/lac/unix/unix_scripts/matrix/update_matrix.sh
@@ -3,4 +3,4 @@ docker-compose -f /root/matrix/docker-compose.yml pull
 docker-compose -f /root/matrix/docker-compose.yml up -d
 
 . ../env.sh
-. update_element_config.sh
+. ./update_element_config.sh
