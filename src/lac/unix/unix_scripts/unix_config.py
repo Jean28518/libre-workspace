9b9c998185a9c0d6ac31151edd3fea50c30c8db2
Jean28518 <jf_v@gmx.de>
2024-04-28 12:09:03 +0200

    Refactor get_value function in unix_config.py to support default parameter

--- src/lac/unix/unix_scripts/unix_config.py
+++ src/lac/unix/unix_scripts/unix_config.py
@@ -34,16 +34,7 @@ def write_config_file():
                 f.write(f"{key}=\"{value}\"\n")
 
 
-def get_value(key):
-    read_config_file()
-    # Get the value of a key from the config file
-    if key in config:
-        return config[key]
-    else:
-        return ""
-    
-
-def get_value(key, default):
+def get_value(key, default=""):
     read_config_file()
     # Get the value of a key from the config file
     if key in config:
