65375398d71a1828139831d81721e36c62e8fef6
Jean28518 <jf_v@gmx.de>
2024-06-15 16:59:49 +0200

    Address issue #92

--- src/lac/unix/unix_scripts/maintenance/cleanup.sh
+++ src/lac/unix/unix_scripts/maintenance/cleanup.sh
@@ -3,3 +3,6 @@ apt autoremove -y
 
 docker image prune -a -f
 docker volume prune -f
+
+# To address the possible memory leak described in https://github.com/Jean28518/libre-workspace/issues/92
+systemctl restart linux-arbeitsplatz-web.service
