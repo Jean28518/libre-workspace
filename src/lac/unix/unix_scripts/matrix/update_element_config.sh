f25eca1491501c74e450396f118609d99eeda361
Jean3219 <jf_v@gmx.de>
2024-06-17 14:14:21 +0200

    Update paths in cp config.json

--- src/lac/unix/unix_scripts/matrix/update_element_config.sh
+++ src/lac/unix/unix_scripts/matrix/update_element_config.sh
@@ -3,14 +3,14 @@
 # Write new element config
 # Change element app/config.json inside the docker container
 echo "Updating element config..."
-docker cp element:/app/config.json /root/matrix/config.json
+docker cp element:/app/config.json /root/matrix/
 sed -i "s#https://matrix-client.matrix.org#https://matrix.$DOMAIN#g" /root/matrix/config.json
 sed -i "s#https://matrix.org#https://matrix.$DOMAIN#g" /root/matrix/config.json
 sed -i "s#matrix.org#matrix.$DOMAIN#g" /root/matrix/config.json
 sed -i "s#meet.element.io#meet.$DOMAIN#g" /root/matrix/config.json
 
 # Copy the changed config.json back to the docker container
-docker cp /root/matrix/config.json element:/app/config.json
+docker cp /root/matrix/config.json element:/app/
 rm /root/matrix/config.json
 
 # Restart the element container
