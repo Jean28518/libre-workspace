797f30001605a6f82d494efebbac39b5abbc437f
Jean28518 <jf_v@gmx.de>
2024-08-01 14:00:09 +0200

    Prevent: OnlyOffice disables itself regulary in Nextcloud #44

--- src/lac/unix/unix_scripts/onlyoffice/update_onlyoffice.sh
+++ src/lac/unix/unix_scripts/onlyoffice/update_onlyoffice.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
 
-# cd /root/onlyoffice
+cd /root/onlyoffice
 
-# docker rm -f onlyoffice
-# bash run.sh
+docker rm -f onlyoffice
+bash run.sh
 
-# We remove and install onlyoffice again, because sometimes the onlyoffice integration in nextcloud stops silently working
-. ../env.sh
-. ../onlyoffice/remove_onlyoffice.sh
-
-cd /usr/share/linux-arbeitsplatz/unix/unix_scripts/onlyoffice
-. ./setup_onlyoffice.sh
-cd /usr/share/linux-arbeitsplatz/unix/unix_scripts/maintenance
+# Set the settings_error to "" to prevent onlyoffice from disabling itself
+sudo -u www-data php /var/www/nextcloud/occ  config:app:set onlyoffice settings_error --value ""
