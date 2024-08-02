ffb6e63072ca19c34916b38c0cbabae703812dfb
Jean28518 <jf_v@gmx.de>
2024-08-02 10:41:09 +0200

    Automatically get the latest netinstall iso from debian project

--- download_debian_iso.sh
+++ download_debian_iso.sh
@@ -1,1 +1,9 @@
-wget -O debian-original.iso https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-12.5.0-amd64-netinst.iso
+# Download the index.html of debian.org
+wget -O /tmp/debian-index.html https://www.debian.org/
+
+#search for the line with https://*-amd64-netinst.iso and extract the link to the latest debian iso
+DOWNLOAD_LINK=$(grep -oP 'href="\K[^"]*debian-[0-9]+.[0-9]+.[0-9]+-amd64-netinst.iso' /tmp/debian-index.html)
+
+echo "Downloading the latest debian iso from: $DOWNLOAD_LINK"
+
+wget -O debian-original.iso $DOWNLOAD_LINK
