673d50691e16aa7b5c083ef697c7a62a8cc3d0a3
Jean28518 <jf_v@gmx.de>
2024-08-02 10:57:01 +0200

    Update Nextcloud update script to include database repair command

--- src/lac/unix/unix_scripts/nextcloud/update_nextcloud.sh
+++ src/lac/unix/unix_scripts/nextcloud/update_nextcloud.sh
@@ -3,3 +3,6 @@ sudo -u www-data php /var/www/nextcloud/occ upgrade
 
 # Update the database
 sudo -u www-data php /var/www/nextcloud/occ db:add-missing-indices
+
+# Repair nextcloud (sometimes this is needed by nextcloud itself.)
+sudo -u www-data php /var/www/nextcloud/occ maintenance:repair --include-expensive
