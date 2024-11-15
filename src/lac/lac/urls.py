6ae35b968d3880ae8ef1e2e94e78c4a9cd6921e0
Jean28518 <jf_v@gmx.de>
2024-11-15 17:42:29 +0100

    Fix [BUG]: Add robots.txt #165

--- src/lac/lac/urls.py
+++ src/lac/lac/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +26,8 @@ urlpatterns = [
     path("welcome/", include("welcome.urls")),
     path("", include("app_dashboard.urls")),
     path('openid/', include('oidc_provider.urls', namespace='oidc_provider')),
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="lac/robots.txt", content_type="text/plain"),
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
