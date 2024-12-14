7cc21d7a7ba2cdcda220d66a6cf94cb3dc805f9c
Jean 28518 <jean28518@github.com>
2024-12-14 12:05:15 +0000

    Fix password variable assignment in django_set_local_Administrator_password.sh

--- django_set_local_Administrator_password.sh
+++ django_set_local_Administrator_password.sh
@@ -5,7 +5,7 @@ if [ -f .env/bin/activate ]; then
     source .env/bin/activate
 fi
 
-$PASSWORD="$1"
+PASSWORD="$1"
 
 # Set the password for the local Administrator user
 python3 manage.py shell -c "from django.contrib.auth.models import User; user = User.objects.get(username='Administrator'); user.set_password('$PASSWORD'); user.save()"
