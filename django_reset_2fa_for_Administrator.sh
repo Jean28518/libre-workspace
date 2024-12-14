f85cbeb0ecf125d7444ebd522f64629689c31478
Jean 28518 <jean28518@github.com>
2024-12-14 13:06:53 +0000

    Update 2FA reset script to use new function and improve password handling in Samba DC removal script

--- django_reset_2fa_for_Administrator.sh
+++ django_reset_2fa_for_Administrator.sh
@@ -11,6 +11,6 @@ if [ -f .env/bin/activate ]; then
     source .env/bin/activate
 fi
 
-python3 manage.py shell -c "from idm.idm import reset_2fa_for; reset_2fa_for('Administrator')"
+python3 manage.py shell -c "from idm.idm import reset_2fa_for_username; reset_2fa_for_username('Administrator')"
 
 echo "2FA for Administrator has been reset"
