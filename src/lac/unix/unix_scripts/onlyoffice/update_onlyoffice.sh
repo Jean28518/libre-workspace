2598366dfa7c0f52b13c1cfb7e58ac3ac1fc80e6
Jean28518 <jf_v@gmx.de>
2025-02-27 13:21:16 +0000

    Update OnlyOffice app within Nextcloud during the update script

--- src/lac/unix/unix_scripts/onlyoffice/update_onlyoffice.sh
+++ src/lac/unix/unix_scripts/onlyoffice/update_onlyoffice.sh
@@ -6,5 +6,8 @@ cd /root/onlyoffice
 docker rm -f onlyoffice
 bash run.sh
 
+# Update onlyoffice app inside nextcloud
+sudo -u www-data php /var/www/nextcloud/occ app:update onlyoffice
+
 # Set the settings_error to "" to prevent onlyoffice from disabling itself
 sudo -u www-data php /var/www/nextcloud/occ  config:app:set onlyoffice settings_error --value ""
