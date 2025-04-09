480935d9284659345c6e594a3d2155dd73a78d59
Jean28518 <jf_v@gmx.de>
2025-04-10 00:34:19 +0100

    Add CPU, RAM, and Disk warning thresholds to miscellaneous settings

--- src/lac/unix/views.py
+++ src/lac/unix/views.py
@@ -603,9 +603,15 @@ def miscellaneous_settings(request):
             else:
                 unix.enable_nextcloud_user_administration()
             unix.set_value("ADDITIONAL_MAIL_ADDRESSES_FOR_SYSTEM_MAILS", form.cleaned_data["additional_mail_addresses_for_system_mails"])
+            unix.set_value("CPU_WARNING_THRESHOLD", form.cleaned_data["cpu_warning_threshold"])
+            unix.set_value("RAM_WARNING_THRESHOLD", form.cleaned_data["ram_warning_threshold"])
+            unix.set_value("DISK_WARNING_THRESHOLD", form.cleaned_data["disk_warning_threshold"])
             return message(request, "Einstellungen gespeichert.", "system_configuration")
     form.fields["disable_nextcloud_user_administration"].initial = not unix.is_nextcloud_user_administration_enabled()
     form.fields["additional_mail_addresses_for_system_mails"].initial = unix.get_value("ADDITIONAL_MAIL_ADDRESSES_FOR_SYSTEM_MAILS", "")
+    form.fields["cpu_warning_threshold"].initial = unix.get_value("CPU_WARNING_THRESHOLD", "80")
+    form.fields["ram_warning_threshold"].initial = unix.get_value("MEMORY_WARNING_THRESHOLD", "80")
+    form.fields["disk_warning_threshold"].initial = unix.get_value("DISK_WARNING_THRESHOLD", "90")
     return render(request, "lac/generic_form.html", {"form": form, "heading": "Verschiedene Einstellungen", "action": "Speichern", "url": reverse("system_configuration"), "hide_buttons_top": "True"})
 
 
