3ed4df0ad28880042a229a806de653b601704035
Jean28518 <jf_v@gmx.de>
2025-05-22 13:53:47 +0100

    Increase maximum backup retention limits in BackupSettings form

--- src/lac/unix/forms.py
+++ src/lac/unix/forms.py
@@ -8,9 +8,9 @@ class BackupSettings(forms.Form):
     borg_encryption = forms.BooleanField(label="Borg Verschlüsselung", required=False, widget=forms.CheckboxInput(attrs={"role": "switch"}), help_text = "\n")
     borg_passphrase = forms.CharField(label="Borg Passphrase (nur bei aktivierter Verschlüsselung auszufüllen)", max_length=100, required=False)
     daily_backup_time = forms.CharField(label="Tägliche Backupzeit (Format: HH:MM)", max_length=100, widget=forms.TextInput(attrs={"placeholder": "02:00"}))
-    keep_daily_backups = forms.IntegerField(label="Anzahl der täglichen Backups", min_value=0, max_value=100, widget=forms.NumberInput(attrs={"placeholder": "7"}))
-    keep_weekly_backups = forms.IntegerField(label="Anzahl der wöchentlichen Backups", min_value=0, max_value=100, widget=forms.NumberInput(attrs={"placeholder": "6"}))
-    keep_monthly_backups = forms.IntegerField(label="Anzahl der monatlichen Backups", min_value=0, max_value=100, widget=forms.NumberInput(attrs={"placeholder": "12"}))
+    keep_daily_backups = forms.IntegerField(label="Anzahl der täglichen Backups", min_value=0, max_value=1000, widget=forms.NumberInput(attrs={"placeholder": "7"}))
+    keep_weekly_backups = forms.IntegerField(label="Anzahl der wöchentlichen Backups", min_value=0, max_value=1000, widget=forms.NumberInput(attrs={"placeholder": "6"}))
+    keep_monthly_backups = forms.IntegerField(label="Anzahl der monatlichen Backups", min_value=0, max_value=1000, widget=forms.NumberInput(attrs={"placeholder": "12"}))
     borg_repo_is_on_synology = forms.BooleanField(label="Borg Repository ist auf einem Synology NAS", required=False, widget=forms.CheckboxInput(attrs={"role": "switch"}), help_text = "\n")
     additional_borg_options = forms.CharField(label="Zusätzliche Borg Optionen (für Experten)", max_length=1000, required=False, widget=forms.TextInput())
 
