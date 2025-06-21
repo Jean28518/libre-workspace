4b4b603e9876df6de869386d50bb39f999bf9454
Jean28518 <jf_v@gmx.de>
2025-06-21 20:24:59 +0100

    Implement API key expiration check in authentication process

--- src/usr/lib/libre-workspace/portal/idm/api_authentication.py
+++ src/usr/lib/libre-workspace/portal/idm/api_authentication.py
@@ -5,9 +5,19 @@ from .models import ApiKey
 
 class ApiKeyAuthentication(authentication.BaseAuthentication):
     def authenticate(self, request):
+        check_api_key_expirations()
         api_key_string = request.META.get('HTTP_API_KEY')
         api_key = ApiKey.objects.filter(key=api_key_string).first()
         if not api_key:
             raise exceptions.AuthenticationFailed('Invalid API key')
         user = api_key.user
         return (user, None)
+    
+
+def check_api_key_expirations():
+    from django.utils import timezone
+    expired_keys = ApiKey.objects.filter(expiration_date__lt=timezone.now())
+    for key in expired_keys:
+        key.delete()
+        print(f"Deleted expired API key: {key.key}")
+    
