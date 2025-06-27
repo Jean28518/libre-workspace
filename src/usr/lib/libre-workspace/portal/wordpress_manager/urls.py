14549f4da7e312928a32b0f95630a8487044d42e
Jean28518 <jf_v@gmx.de>
2025-06-27 23:25:08 +0100

    Fix type hint in URL pattern for deleting WordPress instance

--- src/usr/lib/libre-workspace/portal/wordpress_manager/urls.py
+++ src/usr/lib/libre-workspace/portal/wordpress_manager/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.wordpress_sites, name="wordpress_sites"),
     path("create_wordpress_instance", views.create_wordpress_instance_view, name="create_wordpress_instance"),
-    # path("edit_wordpress_instance/<int:entry_id>", views.edit_wordpress_instance, name="edit_wordpress_instance"),
-    path("delete_wordpress_instance/<int:entry_id>", views.delete_wordpress_instance_view, name="delete_wordpress_instance"),
+    # path("edit_wordpress_instance/<str:entry_id>", views.edit_wordpress_instance, name="edit_wordpress_instance"),
+    path("delete_wordpress_instance/<str:entry_id>", views.delete_wordpress_instance_view, name="delete_wordpress_instance"),
 ]
