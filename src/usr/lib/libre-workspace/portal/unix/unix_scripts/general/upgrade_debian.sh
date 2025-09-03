fa8b770be519220188584233c20d7c1be0163190
Jean28518 <jf_v@gmx.de>
2025-09-03 10:39:50 +0100

    Fix upgrade script to correctly check for 'bookworm' in /etc/os-release and reorganize PHP installation logic

--- src/usr/lib/libre-workspace/portal/unix/unix_scripts/general/upgrade_debian.sh
+++ src/usr/lib/libre-workspace/portal/unix/unix_scripts/general/upgrade_debian.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check if we are currently on bookworm
-if ! grep -q 'bookworm' /etc/apt/sources.list; then
+if ! grep -q 'bookworm' /etc/os-release; then
   echo "Not on bookworm. Skipping Debian Upgrade."
   exit 0
 fi
@@ -33,9 +33,29 @@ DEBIAN_FRONTEND=noninteractive apt autoremove -y
 # Reinstall libre-workspace-portal (because venv of python gets destroyed)
 DEBIAN_FRONTEND=noninteractive apt install --reinstall libre-workspace-portal -y
 
-# Set PHP variables
-. /usr/lib/libre-workspace/modules/nextcloud/set_php_variables.sh
 
+# Check if php is installed:
+if [ -f /usr/bin/php ]; then
+  echo "PHP is installed. Reinstalling PHP packages."
+
+  DEBIAN_FRONTEND=noninteractive sudo apt install php-curl -y
+
+  # Set PHP variables
+  . /usr/lib/libre-workspace/modules/nextcloud/set_php_variables.sh
+fi
+
+# Check if nextcloud is installed
+if [ -d /var/www/nextcloud ]; then
+  echo "Nextcloud is installed. Running maintenance script."
+  sudo -u www-data php /var/www/nextcloud/occ upgrade
+  sudo -u www-data php /var/www/nextcloud/occ app:update --all
+  sudo -u www-data php /var/www/nextcloud/occ maintenance:mode --off
+fi
+
+# If samba_dc should be installed, make sure it is still installed
+if [ -f /root/samba_dc ]; then
+  DEBIAN_FRONTEND=noninteractive sudo apt install samba-ad-dc -y
+fi
 
 # Launch a process in the background sleeping for 1h, then reboot
 echo "Finished Debian Upgrade. Rebooting in 1 hour..."
