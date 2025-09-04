df8e3c30d6a2a0c8786a97193e184fb8b52e7f33
Jean28518 <jf_v@gmx.de>
2025-09-05 00:50:02 +0100

    Add custom CSS for improved documentation styling

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,6 +33,11 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
+html_static_path = ['_static']
+html_css_files = [
+    'css/custom.css',
+]
+
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
